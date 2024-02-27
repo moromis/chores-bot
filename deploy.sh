c5c37b8d2842f4ba7ca517cb7dce0f0e35f6d0ca
Kevin Ulrich <kzulrich@gmail.com>
2024-02-27 15:37:43 -0800

    feat: move to proxy lambda infrastructure

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,6 @@
-rm lambda.zip
-cd ./lambda
-zip -r ../lambda.zip *
-aws lambda update-function-code --no-cli-pager \
-    --function-name bot \
-    --zip-file fileb://../lambda.zip
-cd ../register_commands
-node register.js
+cd sam-app
+npm install
+node generate_template/generate.js
+node register_commands/register.js
+sam build
+sam deploy
---
+++ sam-app/generate_template/misc/templates.js
@@ -0,0 +1,31 @@
+exports.templateResource = (module, command, name) => {
+  return {
+    Type: "AWS::Serverless::Function",
+    Properties: {
+      Handler: `src/modules/${module}/${command.replace(".js", "")}.handler`,
+      Runtime: "nodejs20.x",
+      Architectures: ["arm64"],
+      MemorySize: 128,
+      Timeout: 100,
+      Policies: ["AWSLambdaBasicExecutionRole"],
+      Events: {
+        SNSEvent: {
+          Type: "SNS",
+          Properties: {
+            Topic: {
+              Ref: "MainSNSTopic",
+            },
+            FilterPolicy: {
+              command: [name],
+            },
+          },
+        },
+      },
+    },
+  };
+};
+
+exports.handleNameChange = (resource, name) => {
+  resource.Properties.Events.SNSEvent.Properties.FilterPolicy.command = [name];
+  return resource;
+};
