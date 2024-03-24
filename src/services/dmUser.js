8eab3f5122b14d1892d979841966097e903dbbb9
Kevin Ulrich <kzulrich@gmail.com>
2024-03-23 20:20:43 -0700

    chore: add dmUser tests

--- src/services/dmUser.js
+++ src/services/dmUser.js
@@ -1,4 +1,5 @@
 exports.dmUser = async (client, userId, message) => {
+  if (!client || !userId || !message) return;
   const user = await client.users.fetch(userId);
   console.log(
     "going to try dming the user... are they a bot though? ",
---
+++ src/services/dmUser.test.js
@@ -0,0 +1,40 @@
+const { dmUser } = require("./dmUser");
+
+describe("dmUser", () => {
+  const sendSpyBot = jest.fn(() => {});
+  const sendSpyHuman = jest.fn(() => {});
+  const fetchSpyBot = jest.fn(() => ({ bot: true, send: sendSpyBot }));
+  const fetchSpyHuman = jest.fn(() => ({ bot: false, send: sendSpyHuman }));
+  const testClient = (fetchSpy) => ({
+    users: {
+      fetch: fetchSpy,
+    },
+  });
+  const testMessage = "test";
+
+  beforeEach(() => {
+    jest.clearAllMocks;
+  });
+
+  it("should do nothing if not given a userId", () => {
+    dmUser(testClient(fetchSpyHuman), undefined, "test");
+    expect(fetchSpyHuman).not.toHaveBeenCalled();
+    expect(fetchSpyHuman).not.toHaveBeenCalled();
+    // fetch.mockClear();
+  });
+
+  it("should fetch users given a userId", () => {
+    dmUser(testClient(fetchSpyHuman), "asdf", testMessage);
+    expect(fetchSpyHuman).toHaveBeenCalledTimes(1);
+  });
+
+  it("should send a message if the user is not a bot", () => {
+    dmUser(testClient(fetchSpyHuman), "asdf", testMessage);
+    expect(sendSpyHuman).toHaveBeenCalledWith(testMessage);
+  });
+
+  it("should not send a message if the user is a bot", () => {
+    dmUser(testClient(fetchSpyBot), "asdf", "double test");
+    expect(sendSpyBot).not.toHaveBeenCalled();
+  });
+});
