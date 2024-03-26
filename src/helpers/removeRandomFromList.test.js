3a3579c48d8b9b9fcd04e4923fb1e7715cea43c7
Kevin Ulrich <kzulrich@gmail.com>
2024-03-26 09:18:41 -0700

    chore: add full test for removeRandomFromList

--- src/helpers/removeRandomFromList.test.js
+++ src/helpers/removeRandomFromList.test.js
@@ -10,4 +10,15 @@ describe("removeRandomFromList", () => {
   it("should return a random value when given a list of values", () => {
     expect(typeof removeRandomFromList([1, 2, 3])).toBe("number");
   });
+  it("should return random values without replacement till the given array is empty", () => {
+    const testArray = [1, 2, 3];
+    const removedElements = [];
+    while (testArray.length > 0) {
+      const removed = removeRandomFromList(testArray);
+      expect(removedElements).not.toContain(removed);
+      removedElements.push(removed);
+    }
+    expect(testArray.length).toEqual(0);
+    expect(removedElements.length).toEqual(3);
+  });
 });
